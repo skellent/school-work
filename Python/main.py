d583b43fedc7c206309a6e5c59b8e351d3523c3e
Skellent <skellent69@gmail.com>
2025-04-01 22:25:54 -0400

    Primera versión funcional de la Base de Datos

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,36 @@
 import streamlit as st
+import sqlite3 as sql
+import pandas as pd
+
+# funciones reutilizables
+def agregar_cliente(nombre, cedula, email, telefono, ultima_compra):
+    consultaSQL.execute('''
+        INSERT INTO clientes (nombre, cedula, email, telefono, lastCompra)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (str(nombre), int(cedula), str(email), int(telefono), str(ultima_compra)))
+    conectorSQL.commit()
+
+def consultar_datos():
+    consultaSQL.execute("SELECT * FROM clientes")
+    return(consultaSQL.fetchall())
+
+# Herramientas para manejo de SQL
+try:
+    conectorSQL = sql.connect("Python/base_datos.db")
+    consultaSQL = conectorSQL.cursor()
+    consultaSQL.execute('''
+        CREATE TABLE IF NOT EXISTS clientes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            cedula INTEGER UNSIGNED NOT NULL,
+            email TEXT,
+            telefono INT UNSIGNED,
+            lastCompra DATE NOT NULL
+        )
+    ''')
+    conectorSQL.commit()
+except ValueError:
+    print(ValueError)
 
 # Personalización del Streamlit
 st.set_page_config(
@@ -19,18 +51,43 @@ st.markdown("""
 st.markdown("# Skell's - Dashboard")
 
 # Creación de Ventanas con nombres
-pestaña1, pestaña2, pestaña3 = st.tabs(["Gestión de Clientes", "Horario de Actividad", "Gráficas de Datos"])
+pestaña1, pestaña2, pestaña3, pestaña4 = st.tabs(["Agregar Clientes", "Gestión de Clientes", "Horario de Actividad", "Gráficas de Datos"])
 
 #Contenido de Cada Pestaña
 with pestaña1:
-    st.header("Gestion de Clientes")
-    st.write("Registro de un nuevo cliente:")
+    st.header("Agregar Clientes")
+    with st.form(key="registrar_cliente"):
+        nombre = st.text_input("Nombre Completo *", key='nombre')
+        cedula = st.text_input("Cédula del Cliente *", key='cedula')
+        email = st.text_input("Email del Cliente", key='email')
+        telefono = st.text_input("Teléfono del Cliente *", key='telefono')
+        compra = st.text_input("Fecha de Ultima compra", key='compra')
+        submit = st.form_submit_button("Registar Cliente")
+        if submit:
+            if not nombre or not cedula or not telefono:
+                st.error("Ingrese todos los campos (*).")
+            else:
+                st.success(f"Cliente {nombre} registrado exitosamente")
+                consultar_datos()
+                agregar_cliente(nombre, cedula, email, telefono, compra)
+                consultar_datos()
 
 with pestaña2:
+    # Visualizador de clientes
+    listaClientes = pd.DataFrame(
+        consultar_datos(),
+        columns=["ID", "Nombre", "Cédula", "Email", "Teléfono", "Ultima Compra"]
+    )
+    # Datos esteticamente mostrados
+    st.title("Listado de Clientes Registrados")
+    st.dataframe(
+        listaClientes, hide_index=True, use_container_width=True
+    )
+
+with pestaña3:
     st.header("Horario de Actividad")
     st.write("Estadísticas de flujo de actividad según el horario:")
 
-with pestaña3:
+with pestaña4:
     st.header("Análsis Visual")
     st.write("Gráficas con todos los datos manejados")
-
